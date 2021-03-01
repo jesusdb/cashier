783600a2fee1f311193286456c8454fca26b1c22
Jesús Di Bari <jesus.dibari@paack.co>
2021-03-01 15:11:04 +0100

    Add more tests

--- spec/cashier/item_validator_spec.rb
+++ spec/cashier/item_validator_spec.rb
@@ -3,21 +3,43 @@
 RSpec.describe Cashier::ItemValidator do
   subject(:item_validator) { Object.include described_class }
 
-  describe '#valid_item?' do
-    let(:item) { double('item', code: 'GR1', price: '10.00') }
+  let(:valid_item) { double('item', code: 'GR1', price: '10.00') }
 
+  describe '#valid_item?' do
     context 'when the item is valid' do
-      subject(:validated_item) { item_validator.valid_item?(item) }
+      subject(:result_of_validation) { item_validator.valid_item?(valid_item) }
 
       it { is_expected.to be true }
     end
 
     context 'when the item is invalid' do
-      subject(:validated_item) { item_validator.valid_item?(invalid_item) }
+      subject(:result_of_validation) { item_validator.valid_item?(invalid_item) }
 
       described_class::ATTRIBUTES_TO_VALIDATE.map do |attribute|
+        # Shared context!
         context "because the #{attribute} is not present" do
-          let(:invalid_item) { item.tap { |i| i.unstub(attribute) } }
+          let(:invalid_item) { valid_item.tap { |i| i.unstub(attribute) } }
+
+          it { is_expected.to be false }
+        end
+      end
+    end
+  end
+
+  described_class::ATTRIBUTES_TO_VALIDATE.map do |attribute|
+    describe "#validate_presence_of_#{attribute}" do
+      context 'when the item is valid' do
+        subject(:result_of_validation) { send("validate_presence_of_#{attribute}", valid_item) }
+
+        it { is_expected.to be true }
+      end
+
+      context 'when the item is invalid' do
+        subject(:result_of_validation) { send("validate_presence_of_#{attribute}", invalid_item) }
+
+        # Shared context!
+        context "because the #{attribute} is not present" do
+          let(:invalid_item) { valid_item.tap { |i| i.unstub(attribute) } }
 
           it { is_expected.to be false }
         end
